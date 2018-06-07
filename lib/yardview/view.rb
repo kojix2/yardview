c5778143eee7492cad507d18ff11e04cb543764e
kojix2 <2xijok@gmail.com>
2018-06-08 00:18:01 +0900

    add glade ui

--- lib/yardview/view.rb
+++ lib/yardview/view.rb
@@ -7,20 +7,39 @@ module Yardview
 		end
 
 		def create_gui
-			win = Gtk::Window.new
+			bd = Gtk::Builder.new
+			path = File.expand_path('../../../resources/yardview.ui' ,__FILE__)
+			bd.add_from_file(path)
+			win = bd.get_object("win")
 			win.set_default_size(800, 600)
 			win.signal_connect("destroy") do 
 				Gtk.main_quit
 				Process.kill(:TERM, @yard)
+				@yard = nil
 			end
-			view = WebKit2Gtk::WebView.new
-			view.load_uri("http://localhost:18808")
-			win.add view
+			at_exit{Process.kill(:TERM, @yard) unless @yard.nil?}
+			box = bd.get_object("box")
+			@view = WebKit2Gtk::WebView.new
+			@view.load_uri("http://localhost:18808")
+			win.add @view, expand:true, fill:true
+			bd.connect_signals {|handler| method(handler)}
 			win.show_all
 		end
 
+		def on_home_clicked
+			@view.load_uri("http://localhost:18808")
+		end
+
+		def on_top_clicked
+			puts "Todo: scroll up"
+		end
+
+		def on_refresh_clicked
+			@view.reload
+		end
+
 		def start_yard_server
-			@yard = spawn("yard server -g -p 18808")
+			@yard = spawn("yard server -g -p 18808 --reload")
 			sleep 1
 		end
 	end
